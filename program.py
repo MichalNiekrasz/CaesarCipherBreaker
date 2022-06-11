5ad86dbf908d14b693b0df25506a9209ac6d1497
Michał Niekrasz <mnierasz@gmail.com>
2022-06-11 23:44:23 +0200

    Init

--- program.py
+++ program.py
@@ -2,11 +2,11 @@ alfabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","
 iloscWystapien = {"a":0,"b":0,"c":0,"d":0,"e":0,"f":0,"g":0,"h":0,"i":0,"j":0,"k":0,"l":0,"m":0,"n":0,"o":0,"p":0,"q":0,"r":0,"s":0,"t":0,"u":0,"v":0,"w":0,"x":0,"y":0,"z":0}
 
 def cezar(napis,naj):
-    global najczestszaLitera
-    global indeksjezyka
-    global indekscezara
+    najczestszaLitera
+    indeksjezyka
+    indekscezara
     indeks = 0
-    global maxilosc
+    maxilosc
     maxilosc = 0
     #sprawdz częstotliwość
     for letter in napis:
